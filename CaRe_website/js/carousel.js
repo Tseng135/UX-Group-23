db874bff3f333ab5c139d99838c3bef6880fd0bc
Tseng135 <njzjy1994@gmail.com>
2024-10-07 21:21:23 +1030

    Home Page Finished

---
+++ CaRe_website/js/carousel.js
@@ -0,0 +1,19 @@
+let itemsPerSlide = 3;
+$('#team-carousel').on('slide.bs.carousel', function (e) {
+  let $e = $(e.relatedTarget);
+  let idx = $e.index();
+  let totalItems = $('.carousel-item').length;
+
+  if (idx >= totalItems - (itemsPerSlide - 1)) {
+    let it = itemsPerSlide - (totalItems - idx);
+    for (let i = 0; i < it; i++) {
+      // append slides to end if necessary
+      if (e.direction == "left") {
+        $('.carousel-item').eq(i).appendTo('.carousel-inner');
+      }
+      else {
+        $('.carousel-item').eq(0).appendTo('.carousel-inner');
+      }
+    }
+  }
+});
